1676cfe0b360686bb8c61a5b4e3b0e800c4fe31e
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-05-21 21:44:36 +0200

    Added info page

--- src/main/react/src/Photobook.jsx
+++ src/main/react/src/Photobook.jsx
@@ -9,6 +9,7 @@ import {
 
 import Home from './photobook/Home';
 import Version from './photobook/Version';
+import Source from './photobook/Source';
 import { Button, Grid } from "@mui/material";
 
 const homepage = '/photobook-demo/home';
@@ -29,8 +30,10 @@ class Photobook extends Component {
 
 					<Grid container spacing={4} columns={{ xs: 16 }}>
 					  <Grid item xs={4}><Link to={homepage}><Button color="primary">Home</Button></Link></Grid>
+					  <Grid item xs={4}><Link to={homepage + "/info"}><Button color="primary">Informazioni</Button></Link></Grid>
 					</Grid>
 					<Routes>
+					    <Route path={homepage + "/info"} element={<Source/>} />
 						<Route path="*" element={<Home />} />
 					</Routes>
 
---
+++ src/main/react/src/photobook/Source.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Source extends Component {
+
+	render() {
+		return <div style={{ margin: '20px' }}>
+			<p>Questa &egrave; solo una applicazione dimostrativa, trovate il codice sorgente al seguente link : 
+			<br/><a href="https://github.com/mttfranci/springboot-photobook">https://github.com/mttfranci/springboot-photobook</a></p>
+		</div>
+	}
+
+}
+
+export default Source;
