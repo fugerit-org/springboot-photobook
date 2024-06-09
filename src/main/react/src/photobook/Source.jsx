39d27d2c085fe89f71c8d0b218ea4c50058bb24c
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-06-09 12:19:14 +0200

    Migrate to fugerit-org

--- src/main/react/src/photobook/Source.jsx
+++ src/main/react/src/photobook/Source.jsx
@@ -5,7 +5,7 @@ class Source extends Component {
 	render() {
 		return <div style={{ margin: '20px' }}>
 			<p>Questa &egrave; solo una applicazione dimostrativa, trovate il codice sorgente al seguente link : 
-			<br/><a href="https://github.com/mttfranci/springboot-photobook">https://github.com/mttfranci/springboot-photobook</a></p>
+			<br/><a href="https://github.com/fugerit-org/springboot-photobook">https://github.com/fugerit-org/springboot-photobook</a></p>
 		</div>
 	}
 
